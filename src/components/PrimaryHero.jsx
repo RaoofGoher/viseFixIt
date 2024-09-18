1ef65470de5761bc9d529bb8ef4011282d993541
Raoof Goher <raoofgohersd@gmail.com>
2024-09-19 03:55:28 +0500

    minor changes

--- src/components/PrimaryHero.jsx
+++ src/components/PrimaryHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroImage from '../assets/viseFixitHero.png'
 const PrimaryHero = () => {
   return (
-    <div className="h-screen bg-cover bg-center w-[100vw] h-[70vh] flex justify-center items-center "  style={{ backgroundImage: `url(${HeroImage})` }}>
+    <div className="h-screen bg-cover bg-center w-[98vw] h-[75vh] flex justify-center items-center "  style={{ backgroundImage: `url(${HeroImage})` }}>
         <div className='text-primaryColor text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, magni.</div>
       
     </div>
