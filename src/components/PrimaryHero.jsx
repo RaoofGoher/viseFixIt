0014a7246e002be970c5f64775391d05eb4b76f8
Raoof Goher <raoofgohersd@gmail.com>
2024-09-19 04:00:43 +0500

    height adjustment

--- src/components/PrimaryHero.jsx
+++ src/components/PrimaryHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HeroImage from '../assets/viseFixitHero.png'
 const PrimaryHero = () => {
   return (
-    <div className="bg-center w-[98vw] h-[45vh] flex justify-center items-center "  style={{ backgroundImage: `url(${HeroImage})` }}>
+    <div className="h-screen bg-cover bg-center w-[98vw] h-[35vh] flex justify-center items-center "  style={{ backgroundImage: `url(${HeroImage})` }}>
         <div className='text-primaryColor text-2xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, magni.</div>
       
     </div>
