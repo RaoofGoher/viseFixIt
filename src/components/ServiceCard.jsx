6ad799f22b016ad19df6d32e165f660647745f4b
Raoof Goher <raoofgohersd@gmail.com>
2024-09-20 03:47:04 +0500

    first five sections completed

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ExploreButton from './ExploreButton';
 import ImageUrl from '../assets/p1.jpg';
 
-const ServiceCard = ({ title, description, icon }) => {
+const ServiceCard = ({ title, description, icon, image }) => {
   return (
-    <div className="relative p-4 border bg-lightColor1 border-gray-300 rounded-lg hover:border-primaryColor hover:bg-secondaryColor hover:text-white overflow-hidden group">
+    <div className="relative p-4 border bg-black border-gray-300 rounded-lg hover:border-primaryColor hover:bg-secondaryColor hover:text-white overflow-hidden group">
       {/* Image that is visible by default */}
       <img
-        src={ImageUrl} // Your image URL
+        src={image} // Your image URL
         alt={title}
-        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-700 ease-in-out opacity-100 group-hover:opacity-0"
+        className="absolute inset-0 w-full h-full object-cover transition-opacity duration-700 ease-in-out opacity-100 group-hover:opacity-0 opacity-70"
       />
 
       {/* Content that appears on hover */}
--- src/components/ServiceGrid.jsx
+++ src/components/ServiceGrid.jsx
@@ -14,25 +14,37 @@ import {
     FaUserShield
   } from 'react-icons/fa';
 
+import Roofing from "../assets/Roofing.jpg"
+import Cleaning from "../assets/Cleaning.jpg"
+import Interior from "../assets/Interior Design.jpg"
+import LandScaping from "../assets/Landscaping.jpg"
+import Plumbing from "../assets/Plumbing.jpg"
+import Electrical from "../assets/Electrical.jpg"
+import Painting from "../assets/Painting.jpg"
+import Flooring from "../assets/Flooring.jpg"
+import HVAC from "../assets/HVAC.jpg"
+import PestControl from "../assets/Pest COntrol.jpg"
+
+
 // Sample array of service data
 const services = [
-    { title: 'Roofing', description: 'High-quality roofing services.', icon:<FaMapMarkerAlt/> },
-    { title: 'Cleaning', description: 'Professional cleaning for your home.', icon:<FaBriefcase/> },
-    { title: 'Interior Design', description: 'Stylish interior design solutions.', icon:<FaHome/> },
-    { title: 'Landscaping', description: 'Beautiful landscaping and garden services.', icon:<FaPaintBrush/> },
-    { title: 'Plumbing', description: 'Expert plumbing services for all your needs.', icon:<FaTools/> },
-    { title: 'Electrical', description: 'Safe and reliable electrical services.', icon:<FaCar/> },
-    { title: 'Painting', description: 'Professional painting services for homes and offices.', icon:<FaShoppingCart/> },
-    { title: 'Flooring', description: 'High-quality flooring installation and maintenance.', icon:<FaNetworkWired/> },
-    { title: 'HVAC', description: 'Heating, ventilation, and air conditioning services.', icon:<FaUserShield/> },
-    { title: 'Pest Control', description: 'Effective pest control services to keep your home safe.', icon:<FaBriefcase/> }
+    { title: 'Roofing', description: 'High-quality roofing services.', icon:<FaMapMarkerAlt/>,img: Roofing },
+    { title: 'Cleaning', description: 'Professional cleaning for your home.', icon:<FaBriefcase/>,img: Cleaning },
+    { title: 'Interior Design', description: 'Stylish interior design solutions.', icon:<FaHome/>,img: Interior },
+    { title: 'Landscaping', description: 'Beautiful landscaping and garden services.', icon:<FaPaintBrush/>,img: LandScaping },
+    { title: 'Plumbing', description: 'Expert plumbing services for all your needs.', icon:<FaTools/>,img:Plumbing },
+    { title: 'Electrical', description: 'Safe and reliable electrical services.', icon:<FaCar/>,img: Electrical },
+    { title: 'Painting', description: 'Professional painting services for homes and offices.', icon:<FaShoppingCart/>,img: Painting },
+    { title: 'Flooring', description: 'High-quality flooring installation and maintenance.', icon:<FaNetworkWired/>,img: Flooring },
+    { title: 'HVAC', description: 'Heating, ventilation, and air conditioning services.', icon:<FaUserShield/>,img: HVAC },
+    { title: 'Pest Control', description: 'Effective pest control services to keep your home safe.', icon:<FaBriefcase/>,img: PestControl }
   ];
 
 const ServicesGrid = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-8 p-4">
       {services.map((service, index) => (
-        <ServiceCard key={index} title={service.title} description={service.description} icon={service.icon} />
+        <ServiceCard key={index} title={service.title} description={service.description} icon={service.icon} image={service.img} />
       ))}
     </div>
   );
