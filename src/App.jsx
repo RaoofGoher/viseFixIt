70957a65e63e547ad843dd25e78d91cb4418da34
Raoof Goher <raoofgohersd@gmail.com>
2024-09-20 23:19:50 +0500

    conflict resolved

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   );
 
   return (
-    // Wrap your RouterProvider with GlobalProvider
+    
     <GlobalProvider>
       <RouterProvider router={router} />
     </GlobalProvider>
