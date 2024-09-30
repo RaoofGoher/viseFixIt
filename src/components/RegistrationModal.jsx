c5c631ea525397a04705dbf5f6436fe14e5937ec
Raoof Goher <raoofgohersd@gmail.com>
2024-10-01 03:52:28 +0500

    responsive registration modal

--- src/components/RegistrationModal.jsx
+++ src/components/RegistrationModal.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { useGlobalContext } from '../context/GlobalContext';
 import { Link } from 'react-router-dom';
-
+import { useMediaQuery } from 'react-responsive'
 const RegistrationModal = () => {
   const { isRegistrationModalOpen, closeRegistrationModal } = useGlobalContext();
+  const isMobile = useMediaQuery({
+    query: '(max-width: 775px)'
+  })
+  const isMobile2 = useMediaQuery({
+    query: '(max-width: 390px)'
+  })
 
   return (
     <>
       {isRegistrationModalOpen && (
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-10">
-          <div className="bg-primaryColor p-6 rounded-lg shadow-lg w-1/3 text-white">
+          <div className={`bg-primaryColor p-6 rounded-lg shadow-lg ${isMobile ? "w-[95vw]" : "w-1/2" } text-white`}>
             <h2 className="text-2xl mb-4 text-center">Register</h2>
 
-            <div className="flex justify-between mb-4">
+            <div className={`flex ${isMobile2 ? "flex-col" : "flex-row"} justify-between mb-4`}>
               <Link to="/customersignup"><button 
                 className="bg-secondaryColor text-white px-4 py-2 rounded-lg w-40 hover:bg-primaryColor hover:border hover:border-white"
                 onClick={closeRegistrationModal}
