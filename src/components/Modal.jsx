729fe10ac1fe845b97a2981a770a32d9210027a4
Raoof Goher <raoofgohersd@gmail.com>
2024-10-01 22:06:41 +0500

    services modal added

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,42 @@
 // Modal.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { useGlobalContext } from '../context/GlobalContext';
+
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const Modal = () => {
   const { isServiceModalOpen, closeServiceModal } = useGlobalContext();
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    if (isServiceModalOpen) {
+      const fetchCategories = async () => {
+        setLoading(true);
+        setError(''); // Reset error state
+
+        try {
+          const response = await axios.get(`${apiUrl}/categories/all`);
+          const data = response.data;
+
+          if (data.success) {
+            setCategories(data.data.categories);
+          } else {
+            setError('Failed to fetch categories');
+          }
+        } catch (error) {
+          console.error('Error fetching categories:', error);
+          setError('Error fetching categories');
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchCategories();
+    }
+  }, [isServiceModalOpen]);
 
   if (!isServiceModalOpen) return null;
 
@@ -17,7 +50,25 @@ const Modal = () => {
           &times;
         </button>
         <h2 className="text-2xl">Services</h2>
-        <p>here is the list of our services.</p>
+        <p>Here is the list of our services:</p>
+
+        {loading ? (
+          <p>Loading categories...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <ul className="mt-4">
+            {categories.length > 0 ? (
+              categories.map((category) => (
+                <li key={category.id} className="my-2">
+                  {category.name}
+                </li>
+              ))
+            ) : (
+              <li>No categories available.</li>
+            )}
+          </ul>
+        )}
       </div>
     </div>
   );
