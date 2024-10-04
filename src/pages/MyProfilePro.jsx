061397021cb8bf8b6a6add0a0b22bcbf7b521f12
Raoof Goher <raoofgohersd@gmail.com>
2024-10-05 01:59:23 +0500

    initial design

--- src/pages/MyProfilePro.jsx
+++ src/pages/MyProfilePro.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useProContext } from '../context/ProContext';
+import axios from 'axios'; // Import axios for making API requests
 
 const MyProfilePro = () => {
-  return (
-    <div>
-      <h1>here is main profile page for pro</h1>
-    </div>
-  )
-}
+  const { proData } = useProContext();
+  const [id, setId] = useState('');
 
-export default MyProfilePro
+  // Set id when proData changes
+  useEffect(() => {
+    if (proData && proData.id) {
+      setId(proData.id);
+    }
+    console.log("proData:", proData);
+  }, [proData]);
+
+  // Fetch data using id
+  useEffect(() => {
+    const fetchData = async () => {
+      if (id) {
+        try {
+          const response = await axios.get(`https://51.20.63.119/service_provider/get/${id}`);
+          console.log("API Response:", response.data.data.service_provider); // Log the response data from the API
+        } catch (error) {
+          console.error('Error fetching service provider data:', error);
+        }
+      }
+    };
+
+    fetchData(); // Fetch data if id is available
+  }, [id]); // Trigger the fetch whenever id changes
+
+  return (
+    <></>
+  );
+};
+
+export default MyProfilePro;
