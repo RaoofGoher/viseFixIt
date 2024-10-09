6bcea6502b449731fc635eb619ef67bf7e290274
Raoof Goher <raoofgohersd@gmail.com>
2024-10-10 03:04:33 +0500

    toast added in pro profile

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 
 const Toast = ({ type, message }) => {
   const toastClasses = classNames(
-    'px-4 py-3 rounded-lg shadow-lg text-white flex items-center space-x-2',
+    'px-4 py-3 rounded-lg shadow-lg text-black flex items-center space-x-2',
     {
       'bg-green-500': type === 'success',
       'bg-red-500': type === 'error',
