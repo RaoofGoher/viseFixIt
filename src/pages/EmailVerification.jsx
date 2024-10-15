17c02e50df6321c47b233a6a5c001fb3313f89f3
Raoof Goher <raoofgohersd@gmail.com>
2024-10-15 19:08:32 +0500

    navigation and toast added

--- src/pages/EmailVerification.jsx
+++ src/pages/EmailVerification.jsx
@@ -1,18 +1,24 @@
-import { useParams } from 'react-router-dom';
+import { useParams,useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useToast } from '../context/ToastContext';
 
 const EmailVerification = () => {
   const { uid, token } = useParams();
   const [status, setStatus] = useState('Verifying...');
+  const navigate = useNavigate();
+  const { showToast } = useToast();
 
   useEffect(() => {
     const verifyEmail = async () => {
       try {
         const response = await axios.get(`https://api.thefixit4u.com/activate/${uid}/${token}/`);
-        console.log("email verifiaction response",response)
         if (response.status === 200) {
           setStatus('Your email has been verified successfully!');
+          showToast('Email verified successfully!','success');
+          setTimeout(() => {
+            navigate("/");
+          }, 2000);
         } else {
           setStatus('Email verification failed.');
         }
@@ -26,7 +32,7 @@ const EmailVerification = () => {
   }, [uid, token]);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
+    <div className="flex items-center justify-center h-[70vh] bg-gray-100">
       <div className="bg-white p-8 rounded shadow-md">
         <h2 className="text-xl font-semibold mb-4">Email Verification</h2>
         <p>{status}</p>
