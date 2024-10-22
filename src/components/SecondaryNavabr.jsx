6bff9c8682b24308f131fc97c460968b2bc50af6
Raoof Goher <raoofgohersd@gmail.com>
2024-10-23 02:54:50 +0500

    minor updates

--- src/components/SecondaryNavabr.jsx
+++ src/components/SecondaryNavabr.jsx
@@ -52,24 +52,24 @@ const SecondaryNavbar = () => {
   const activeLinkStyle = { 
     
     backgroundColor:"#282829",
-    paddingTop:'100px',
-    paddingBottom:'100px',
+    paddingTop:'30px',
+    paddingBottom:'30px',
     paddingLeft:"10px",
     paddingRight:"10px",
   };
   
   return (
     <>
-      <nav className="bg-primaryColor w-[94vw] mx-auto mb-2 relative rounded ">
+      <nav className="bg-primaryColor w-[94vw] mx-auto mb-2 relative rounded">
         <button
           onClick={scrollLeft}
-          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-primaryColor p-2 rounded-full hover:bg-gray-200 transition"
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-primaryColor p-2 rounded-full hover:bg-secondaryColor transition"
         >
-          <FaChevronLeft className='text-secondaryColor font-bold text-2xl' />
+          <FaChevronLeft className='text-white font-bold text-2xl' />
         </button>
         <div
           ref={scrollRef}
-          className="flex overflow-x-auto space-x-4 p-4 items-center scrollbar-hide w-full"
+          className="flex overflow-x-auto space-x-4 p-4 items-center w-full h-[80px]"
           style={{ scrollbarWidth: 'none' }} // For Firefox
         >
           <ul className="flex space-x-4 list-none ml-6">
@@ -78,7 +78,7 @@ const SecondaryNavbar = () => {
                 <NavLink
                 to={`/category/${item.id}`}
                   onClick={() => handleCategoryClick(item)} // Handle click event
-                  className={`text-black font-bold transition duration-200 text-white hover:text-secondaryColor`}
+                  className={`text-black font-bold transition duration-200 text-white hover:text-white`}
                   style={({ isActive }) => (isActive ? activeLinkStyle : null)}
                 >
                   {item.name}
@@ -89,9 +89,9 @@ const SecondaryNavbar = () => {
         </div>
         <button
           onClick={scrollRight}
-          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-primaryColor p-2 rounded-full  hover:bg-gray-200 transition"
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-primaryColor p-2 rounded-full  hover:bg-secondaryColor transition"
         >
-          <FaChevronRight className='text-secondaryColor font-bold text-2xl' />
+          <FaChevronRight className='text-white font-bold text-2xl' />
         </button>
       </nav>
     </>
