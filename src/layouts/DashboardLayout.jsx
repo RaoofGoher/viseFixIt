7d058e18fa421d0be7e9a3845f74333cc7ee4a70
Raoof Goher <raoofgohersd@gmail.com>
2024-10-22 22:57:14 +0500

    dashboard work in progress

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,24 +1,29 @@
 // DashboardLayout.jsx
 import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet, NavLink } from 'react-router-dom';
 import PrimaryNavbar from '../components/PrimaryNavbar';
 
 const DashboardLayout = () => {
+
+    const activeLinkStyle = { 
+        backgroundColor:"#F58634"
+      };
+    
     return (
-        <>
+        <div>
             <PrimaryNavbar />
-            <div className="flex w-[90vw]">
-                <aside className="w-64 h-screen bg-gray-800 text-white">
+            <div className="flex w-[90vw] ml-[40px]">
+                <aside className="w-64 h-screen bg-secondaryColor text-white">
                     <nav>
                         <ul>
                             <li>
-                                <Link to="/dashboard/home" className="block p-4 hover:bg-gray-700">Home</Link>
+                                <NavLink to="/dashboard/home" className="block p-4 hover:bg-primaryColor"  style={({ isActive }) => (isActive ? activeLinkStyle : null)}>Home</NavLink>
                             </li>
                             <li>
-                                <Link to="/dashboard/profile" className="block p-4 hover:bg-gray-700">Profile</Link>
+                                <NavLink to="/dashboard/profile" className="block p-4 hover:bg-primaryColor" style={({ isActive }) => (isActive ? activeLinkStyle : null)}>Profile</NavLink>
                             </li>
                             <li>
-                                <Link to="/dashboard/settings" className="block p-4 hover:bg-gray-700">Settings</Link>
+                                <NavLink to="/dashboard/settings" className="block p-4 hover:bg-primaryColor"style={({ isActive }) => (isActive ? activeLinkStyle : null)} >Settings</NavLink>
                             </li>
                             {/* Add more links as needed */}
                         </ul>
@@ -28,7 +33,7 @@ const DashboardLayout = () => {
                     <Outlet /> {/* This will render the matched child routes */}
                 </main>
             </div>
-        </>
+        </div>
 
     );
 };
